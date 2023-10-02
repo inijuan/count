6921a303f308704272b6e5a20900669c3cf77dbe
inijuan <133223431+inijuan@users.noreply.github.com>
2023-10-03 01:21:47 +0700

    Add files via upload

---
+++ count/class-counter.js
@@ -0,0 +1,31 @@
+class Count extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            count: 0,
+        };
+    }
+
+    increaseCount = () => {
+        this.setState({count: this.state.count + 1 });
+    };
+
+    decreaseCount = () => {
+        this.setState({count: this.state.count - 1});
+    };
+
+    render() {
+        return (
+            <div>
+                <h2><strong>{this.state.count}</strong></h2>
+                <div>
+                <button onClick={this.decreaseCount}>-1</button>
+                <button onClick={this.increaseCount}>+1</button>
+                </div>
+            </div>
+        
+        )
+    }
+}
+ReactDOM.render(<Count />, document.getElementById('Count'));
+
